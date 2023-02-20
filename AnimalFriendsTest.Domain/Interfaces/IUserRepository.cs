d4648d2ed584da65e1b514f3de041a0bce52483f
Chris rabe <chris_rabe@yahoo.co.uk>
2023-02-20 08:49:20 +0000

    Task :  add EF packages and respository
    currently no implementation for user repo

---
+++ AnimalFriendsTest.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AnimalFriendsTest.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriendsTest.Domain.Interfaces
+{
+	public interface IUserRepository : IGenericRepository<User>
+	{
+	}
+}
---
+++ AnimalFriendsTest.Domain/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AnimalFriendsTest.Domain.Interfaces;
+using AnimalFriendsTest.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriendsTest.Domain.Repository
+{
+	internal class UserRepository : IUserRepository
+	{
+		public int Add(User Entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
